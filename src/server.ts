d5fb0019b5584eff9200755af55040cf0ff902b3
DanielNasc <danielnasc15987@gmail.com>
2022-02-28 16:39:54 -0300

    iniciando a documentacao

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+
 import { router } from "./routes";
+
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (_, res) => res.json({ message: "Hallo warudo" }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(router);
 
 app.listen(3000, () => console.log("server running"));
